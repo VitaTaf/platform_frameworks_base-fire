7b8238c3c4117607a0d2586f6248a5bd2189546a
Chong Zhang <chz@google.com>
2015-01-05 11:25:39 -0800

    fix the flash caused by missing setMatrix() when surface size changes
    bug: 18773834
    
    Change-Id: I16e2f896e6fd70e9b130bb55ecefa8c2f08c684a

--- services/core/java/com/android/server/wm/WindowStateAnimator.java
+++ services/core/java/com/android/server/wm/WindowStateAnimator.java
@@ -1370,7 +1370,7 @@ class WindowStateAnimator {
         }
     }
 
-    void setSurfaceBoundariesLocked(final boolean recoveringMemory) {
+    private void setSurfaceBoundariesLocked(final boolean recoveringMemory) {
         final WindowState w = mWin;
 
         int width;
