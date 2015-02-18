2ef8dbc15c38d4114f0a67070f5fca89c07c4e97
Mohamad Ayyash <mkayyash@google.com>
2015-02-18 12:16:17 -0800

    AlsaCardsParser: Ensure line is not NULL when reading Cards File.
    Change-Id: I5136bbb776bbbf97229b2d96975b819a94232d66
    Signed-off-by: Mohamad Ayyash <mkayyash@google.com>

--- core/java/android/alsa/AlsaCardsParser.java
+++ core/java/android/alsa/AlsaCardsParser.java
@@ -114,6 +114,9 @@ public class AlsaCardsParser {
                 cardRecord.parse(line, 0);
 
                 line = bufferedReader.readLine();
+                if (line == null) {
+                    break;
+                }
                 if (DEBUG) {
                     Slog.i(TAG, "  " + line);
                 }
