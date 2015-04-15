8e27793dd312a6b4879cc2a8ce5c5aad62705058
Selim Cinek <cinek@google.com>
2015-04-14 20:04:12 -0700

    Added the heads up scrim back
    Also fixed a bug where the notification panel would
    keep a hardware layer.
    Also improved the interal structure of the HeadsUpManager
    
    Change-Id: I635e4a89b0a32573c26515cc3e65e142d77593ec

--- packages/SystemUI/src/com/android/systemui/statusbar/stack/StackStateAnimator.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/stack/StackStateAnimator.java
@@ -774,7 +774,7 @@ public class StackStateAnimator {
         };
     }
 
-    private static <T> T getChildTag(View child, int tag) {
+    public static <T> T getChildTag(View child, int tag) {
         return (T) child.getTag(tag);
     }
 
