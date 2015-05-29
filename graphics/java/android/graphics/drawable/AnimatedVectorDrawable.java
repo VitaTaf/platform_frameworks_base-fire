d7231a68abb3735e3e9fb1c254f030d4b5b57c67
Alan Viverette <alanv@google.com>
2015-05-29 15:12:14 -0700

    Replace PNG-based seekbar thumb animation with AVD
    Adds optical inset support for VectorDrawable and GradientDrawable.
    
    Bug: 19944181
    Change-Id: I9df04d9fe17ad858413e7f93694bf37ee2c43c85

--- graphics/java/android/graphics/drawable/AnimatedVectorDrawable.java
+++ graphics/java/android/graphics/drawable/AnimatedVectorDrawable.java
@@ -23,6 +23,7 @@ import android.content.res.Resources.Theme;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
+import android.graphics.Insets;
 import android.graphics.Outline;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
@@ -265,6 +266,12 @@ public class AnimatedVectorDrawable extends Drawable implements Animatable {
     @Override
     public void getOutline(@NonNull Outline outline) {
         mAnimatedVectorState.mVectorDrawable.getOutline(outline);
+    }
+
+    /** @hide */
+    @Override
+    public Insets getOpticalInsets() {
+        return mAnimatedVectorState.mVectorDrawable.getOpticalInsets();
     }
 
     @Override
